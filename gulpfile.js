42d807d0a0c789f3edbaaee6d7a7559216518037
Mario Sánchez <mariojose1693@gmail.com>
2021-01-27 06:15:08 -0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+// 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglifycss = require('gulp-uglifycss');
+// -------------------------------------
+gulp.task('sass', () => {
+    return gulp.src('./resources/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(uglifycss())
+    .pipe(gulp.dest('./public/css'));
+});
+// watch
+gulp.task('watch', () => {
+    gulp.watch('./resources/sass/**/*.scss', gulp.series('sass'));
+  });
+  
+  
+gulp.task('default', gulp.parallel( 'sass','watch'));
